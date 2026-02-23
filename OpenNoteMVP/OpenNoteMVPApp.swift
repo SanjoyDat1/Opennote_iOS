837f459f361185167f191325c9978af9d585a0ec
SanjoyDat1 <sanjoy1.datta1@gmail.com>
2026-02-23 12:37:55 -0800

    Initial Commit

---
+++ OpenNoteMVP/OpenNoteMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenNoteMVPApp.swift
+//  OpenNoteMVP
+//
+//  Created by Sanjoy Datta on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct OpenNoteMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
